aa417c0e5ede31918e6afa695f52e741a5afeba6
Mahmoudali55 <mahmoud7491538@std.mans.edu.eg>
2026-01-22 08:41:30 +0300

    done_login

--- lib/core/network/app_interceptors.dart
+++ lib/core/network/app_interceptors.dart
@@ -1,21 +1,20 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
+
+import 'package:my_template/core/cache/hive/hive_methods.dart';
+
 import '../utils/common_methods.dart';
 
 class AppInterceptors extends Interceptor {
   AppInterceptors();
   static bool isInternet = true;
   @override
-  Future<void> onRequest(
-    RequestOptions options,
-    RequestInterceptorHandler handler,
-  ) async {
+  Future<void> onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
     isInternet = true;
-    debugPrint('REQUEST[${options.method}] => PATH: ${options.path}');
 
-    options.headers['Content-Type'] = 'application/json';
-    options.headers['Accept'] = 'application/json';
-    options.headers['x-api-key'] = 'reqres-free-v1';
+    options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+
+    final lang = HiveMethods.getLang();
+    options.headers['lang'] = lang == 'en' ? 'en-GB' : lang;
 
     // Check internet connectivity before sending request
     if (!await CommonMethods.hasConnection()) {
@@ -30,23 +29,5 @@ class AppInterceptors extends Interceptor {
     }
 
     super.onRequest(options, handler);
-  }
-
-  @override
-  void onResponse(Response response, ResponseInterceptorHandler handler) {
-    debugPrint(
-      'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}',
-    );
-
-    super.onResponse(response, handler);
-  }
-
-  @override
-  void onError(DioException err, ErrorInterceptorHandler handler) {
-    debugPrint(
-      'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}',
-    );
-
-    super.onError(err, handler);
   }
 }
--- lib/core/network/handle_dio_request.dart
+++ lib/core/network/handle_dio_request.dart
@@ -4,20 +4,16 @@ import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:my_template/core/error/failures.dart';
 
-Future<Either<Failure, T>> handleDioRequest<T>({
-  required Future<T> Function() request,
-  String? message,
-}) async {
+Future<Either<Failure, T>> handleDioRequest<T>({required Future<T> Function() request}) async {
   try {
     final response = await request();
     return Right(response);
   } on DioException catch (e) {
-    // Optional: log the Dio error
     log('[DioException] ➜ ${e.message}');
-    log(e.response?.data['Message']);
+    log('[Response] ➜ ${e.response?.data}');
+
     return Left(ServerFailure.fromDioError(e));
   } catch (e, stack) {
-    // Optional: log unknown errors
     log('[Unknown Error] ➜ $e');
     log('[StackTrace] ➜ $stack');
 
