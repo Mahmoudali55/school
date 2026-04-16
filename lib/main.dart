b8efd2a4fdd74be193f4ddf28f3ee5d6d7d56676
asggroups56-svg <asggroups56@gmail.com>
2026-04-16 11:36:12 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'app.dart';
 import 'core/services/push_notification_service.dart';
 import 'core/theme/cubit/app_theme_cubit.dart';
 import 'firebase_options.dart';
 import 'service_initialize.dart';
-
 @pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   print("Handling a background message: ${message.messageId}");
 }
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
@@ -23,7 +20,6 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await PushNotificationService().initialize();
-
   runApp(
     EasyLocalization(
       supportedLocales: const [Locale('ar'), Locale('en')],
